614b25c66ed10842a6ff898c136583cc308153c8
ShrajalBajpai <bajpaishrajal368@gmail.com>
2026-08-26 14:35:46 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://xhtnidoouiaolljkqsus.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-    "YOUR_PUBLISHABLE_KEY_HERE";
+    "sb_publishable_W-F-EydBhQh-rXBoBaHCUw_X-7peEiC";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
